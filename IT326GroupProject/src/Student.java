e62f82257e49b698fdc8e86cf396215b17dd4a2c
Powaful3000 <davidc0330@gmail.com>
2024-10-29 20:06:55 -0500

    remove hw

---
+++ IT326GroupProject/src/Student.java
@@ -0,0 +1,23 @@
+
+public class Student {
+	private Integer userID;
+	private String userName;
+	private String userYear;
+	private String[] userTags;
+	
+	public Student(Integer userID, String userName, String userYear, String[] userTags) {
+		
+	}
+	
+	public Integer getID() {
+		
+	}
+	
+	public String getName() {}
+	public String getYear() {}
+	public Boolean addTag(String tagName) {}
+	public Boolean editTag(String tag) {}
+	public Boolean deleteTag(String tag) {}
+	
+	
+}
