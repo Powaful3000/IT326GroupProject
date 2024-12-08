71bd26a52b812a5f4bf3a7fe13aec614554d4b0c
xzamora425 <xzamora425@gmail.com>
2024-12-08 17:45:00 -0600

    starting testing

--- src/RedbirdConnectTest.java
+++ src/RedbirdConnectTest.java
@@ -1,4 +1,34 @@
-public class RedbirdConnectTest {
-    @Test
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 
+public class RedbirdConnectTest {
+
+    private final DatabaseHandler dbHandler;
+
+    @Before
+    public void init() {
+        List<Tag> xavierTags = Arrays.asList(
+            new Tag("IT"),
+            new Tag("OffCampus")
+        );
+        List<Group> xavierGroups = Arrays.asList(
+            new Group("IT326"),
+            new Group("IT355")
+        );
+        List<Post> xavierPosts = new ArrayList<>();
+
+        Student testStudent = new Student(
+            0,
+            "xzamora@ilstu.edu",
+            "Xavier Zamora",
+            "Senior",
+            xavierTags,
+            xavierGroups,
+            xavierPosts
+        );
+    }
+
+    @Test
+    public void testingAccountExistsWhenRegistering() {}
 }
