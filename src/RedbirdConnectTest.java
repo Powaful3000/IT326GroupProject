6f24b2a950d110ea87ae2638c620ac497338d8f1
Powaful3000 <davidc0330@gmail.com>
2024-12-08 18:39:25 -0600

    test examples

--- src/RedbirdConnectTest.java
+++ src/RedbirdConnectTest.java
@@ -1,6 +1,8 @@
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
+
+import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
 import static org.junit.Assert.*;
@@ -9,15 +11,20 @@ public class RedbirdConnectTest {
 
     public DatabaseHandler dbHandler;
     public StudentController studentController;
+    public Tag t1, t2;
+    public Group g1, g2;
+    public List<Tag> tags;
+    public List<Group> groups;
 
     @Before
     public void init() {
-        Tag t1 = new Tag(0, "name", "description");
-        Tag t2;
-        Group g1 = new Group(0, "name", "description");
-        Group g2;
-        List<Tag> tags = Arrays.asList(t1,t2);
-        List<Group> groups = Arrays.asList(g1,g2);
+        t1 = new Tag(0, "name", "description");
+        t2 = new Tag(1,"name2","desc2");
+        g1 = new Group(0, "name", "description");
+        g2 = new Group(1, "name2", "description2"); 
+
+        tags = Arrays.asList(t1,t2);
+        groups = Arrays.asList(g1,g2);
 
 
         List<Tag> xavierTags = Arrays.asList(
@@ -44,5 +51,19 @@ public class RedbirdConnectTest {
     @Test
     public void testingAccountExistsWhenRegistering() {
         assertFalse(studentController.registerStudent("Xavier Zamora", "Senior", 0));
+    }
+
+    // Cleanup for gc
+    @After
+    public void cleanUp() {
+        dbHandler = null;
+        studentController = null;
+        t1 = null;
+        t2 = null;
+        g1 = null;
+        g2 = null;
+        tags = null;
+        groups = null;
+
     }
 }
